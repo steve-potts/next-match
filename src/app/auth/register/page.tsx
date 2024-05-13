de1ff421556dd1dd9dc64ab3cacfdc593894aa21
Steve Potts <coolpot_1@yahoo.com>
2024-05-13 23:23:52 +0100

    prisma config before db init

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import RegisterForm from './RegisterForm'
 
 export default function RegisterPage() {
   return (
-    <div>
-      Register
+    <div className='flex items-center justify-center vertical-center'>
+      <RegisterForm />
     </div>
+    
   )
 }
